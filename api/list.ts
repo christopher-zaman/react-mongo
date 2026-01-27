97d8bd9b60d9cdf7149ca9601352f7d79c0aef7a
Christopher Zaman <zamanchristopher@gmail.com>
2026-01-26 19:59:16 -0500

    extension

--- api/list.ts
+++ api/list.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import clientPromise from "./_db.ts";
+import clientPromise from "./_db";
 console.log("MONGODB_URI present:", !!process.env.MONGODB_URI);
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
--- api/submit.ts
+++ api/submit.ts
@@ -1,5 +1,5 @@
 import type { VercelRequest, VercelResponse } from "@vercel/node";
-import clientPromise from "./_db.ts";
+import clientPromise from "./_db";
 
 export default async function handler(req: VercelRequest, res: VercelResponse) {
   if (req.method !== "POST") {
